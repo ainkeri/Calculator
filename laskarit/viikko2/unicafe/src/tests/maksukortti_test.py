873d023917829114f66c10c3e254e625659e1dc9
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-03-21 12:31:28 +0200

    tehtävä 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_rahan_lataus_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 12.00 euroa")
+
+    def test_maksukortilta_voi_ottaa_rahaa(self):
+        self.maksukortti.ota_rahaa(200)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_maksukortti_ei_mene_miinukselle(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
