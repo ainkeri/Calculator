2531f8d79e9322b4db39d52207eceff67b9e4dd7
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-03-30 12:45:07 +0300

    src luominen ja calculator.py

---
+++ calculator/src/calc/calculator.py
@@ -0,0 +1,27 @@
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+
+    def substract(self, num):
+        self.result -= num
+
+    def multiply(self, num):
+        self.result *= num
+
+    def divide(self, num):
+        if num != 0:
+            self.result /= num
+        else:
+            print("Error: Cannot divide by zero")
+    
+    def clear(self):
+        self.result = 0
+
+    def get_result(self):
+        return self.result
+
+
+calc = Calculator()
