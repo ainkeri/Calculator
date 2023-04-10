c2b6ad0e54220b58e7971960178680f56998fc93
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-04-10 12:58:53 +0300

    layout

--- calculator/src/calc/calculator_app.py
+++ calculator/src/calc/calculator_app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from src.calc.calculator import Calculator
 
+
 class CalculatorApp:
     def __init__(self):
         self.calculator = Calculator()
@@ -14,41 +15,51 @@ class CalculatorApp:
         self.text = tk.Text(self.root, height=2, width=16, font=("Arial", 22))
         self.text.grid(columnspan=5)
 
-        self.create_button("1", 1, 0)
-        self.create_button("2", 1, 1)
-        self.create_button("3", 1, 2)
-        self.create_button("4", 2, 0)
-        self.create_button("5", 2, 1)
-        self.create_button("6", 2, 2)
-        self.create_button("7", 3, 0)
-        self.create_button("8", 3, 1)
-        self.create_button("9", 3, 2)
-        self.create_button("0", 4, 1)
+        tk.Button(self.root, text="1", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("1")).grid(row=1, column=0)
+        tk.Button(self.root, text="2", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("2")).grid(row=1, column=1)
+        tk.Button(self.root, text="3", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("3")).grid(row=1, column=2)
+        tk.Button(self.root, text="4", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("4")).grid(row=2, column=0)
+        tk.Button(self.root, text="5", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("5")).grid(row=2, column=1)
+        tk.Button(self.root, text="6", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("6")).grid(row=2, column=2)
+        tk.Button(self.root, text="7", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("7")).grid(row=3, column=0)
+        tk.Button(self.root, text="8", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("8")).grid(row=3, column=1)
+        tk.Button(self.root, text="9", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("9")).grid(row=3, column=2)
+        tk.Button(self.root, text="0", height=2, width=6, font=(
+            "Arial", 18), command=lambda: self.button_click("0")).grid(row=4, column=1)
 
-        self.create_button("+", 1, 3)
-        self.create_button("-", 2, 3)
-        self.create_button("*", 3, 3)
-        self.create_button("/", 4, 3)
+        tk.Button(self.root, text="+", height=2, width=6, font=("Arial", 18),
+                  command=lambda: self.button_click("+")).grid(row=1, column=3)
+        tk.Button(self.root, text="-", height=2, width=6, font=("Arial", 18),
+                  command=lambda: self.button_click("-")).grid(row=2, column=3)
+        tk.Button(self.root, text="*", height=2, width=6, font=("Arial", 18),
+                  command=lambda: self.button_click("*")).grid(row=3, column=3)
+        tk.Button(self.root, text="/", height=2, width=6, font=("Arial", 18),
+                  command=lambda: self.button_click("/")).grid(row=4, column=3)
 
-        self.create_button("C", 4, 0)
-        self.create_button("=", 4, 2)
+        tk.Button(self.root, text="C", height=2, width=6, font=(
+            "Arial", 18), command=self.calculator.clear()).grid(row=4, column=0)
+        tk.Button(self.root, text="=", height=2, width=6, font=(
+            "Arial", 18), command=self.calculator.clear()).grid(row=4, column=2)
 
         self.root.mainloop()
-    
-    def create_button(self, text, row, column):
-        button = tk.Button(self.root, text=text, height=2, width=6, font=("Arial", 18), command=lambda: self.button_click(text))
-        button.grid(row=row, column=column)
 
+    # next function doesn't work yet
     def button_click(self, text):
         if text == "C":
-            self.clear()
+            self.calculator.clear()
         elif text == "=":
-            self.result()
-
+            self.calculator.get_result()
+        else:
+            pass
 
 
 app = CalculatorApp()
-        
-
-
-
