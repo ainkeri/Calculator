704d5e55f102945b06b77ba83f0fb7c3d6c88901
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-04-22 16:19:11 +0300

    calculator.pyn muokkaus toimivammaksi calculator_appin kanssa

--- calculator/src/calc/calculator.py
+++ calculator/src/calc/calculator.py
@@ -1,24 +1,28 @@
 class Calculator:
     def __init__(self):
+        self.a = 0
+        self.b = ""
         self.result = 0
 
     def add(self, num):
-        self.result += num
+        self.result = self.a + num
 
     def substract(self, num):
-        self.result -= num
+        self.result = self.a - num
 
     def multiply(self, num):
-        self.result *= num
+        self.result = self.a * num
 
     def divide(self, num):
         if num != 0:
-            self.result /= num
+            self.result = self.a / num
         else:
             self.result = None
             print("Error: Cannot divide by zero")
 
     def clear(self):
+        self.a = 0
+        self.b = ""
         self.result = 0
 
     def get_result(self):
