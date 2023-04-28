c7f263dc3b25101afe8e100eb2793a55b5bda251
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-04-28 17:06:10 +0300

    calculator appin muokkausta

--- calculator/src/application/calculator_app.py
+++ calculator/src/application/calculator_app.py
@@ -15,42 +15,32 @@ class CalculatorApp:
         self.text = tk.Text(self.root, height=2, width=16, font=("Arial", 22))
         self.text.grid(columnspan=5)
 
-        tk.Button(self.root, text="1", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("1")).grid(row=1, column=0)
-        tk.Button(self.root, text="2", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("2")).grid(row=1, column=1)
-        tk.Button(self.root, text="3", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("3")).grid(row=1, column=2)
-        tk.Button(self.root, text="4", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("4")).grid(row=2, column=0)
-        tk.Button(self.root, text="5", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("5")).grid(row=2, column=1)
-        tk.Button(self.root, text="6", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("6")).grid(row=2, column=2)
-        tk.Button(self.root, text="7", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("7")).grid(row=3, column=0)
-        tk.Button(self.root, text="8", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("8")).grid(row=3, column=1)
-        tk.Button(self.root, text="9", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("9")).grid(row=3, column=2)
-        tk.Button(self.root, text="0", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("0")).grid(row=4, column=1)
-
-        tk.Button(self.root, text="+", height=2, width=6, font=("Arial", 18),
-                  command=lambda: self.button_click("+")).grid(row=1, column=3)
-        tk.Button(self.root, text="-", height=2, width=6, font=("Arial", 18),
-                  command=lambda: self.button_click("-")).grid(row=2, column=3)
-        tk.Button(self.root, text="*", height=2, width=6, font=("Arial", 18),
-                  command=lambda: self.button_click("*")).grid(row=3, column=3)
-        tk.Button(self.root, text="/", height=2, width=6, font=("Arial", 18),
-                  command=lambda: self.button_click("/")).grid(row=4, column=3)
-
-        tk.Button(self.root, text="C", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("C")).grid(row=4, column=0)
-        tk.Button(self.root, text="=", height=2, width=6, font=(
-            "Arial", 18), command=lambda: self.button_click("=")).grid(row=4, column=2)
+        self.create_button()
 
         self.root.mainloop()
+
+    def create_button(self):
+        buttons = [
+        {"text": "1", "row": 1, "column": 0},
+        {"text": "2", "row": 1, "column": 1},
+        {"text": "3", "row": 1, "column": 2},
+        {"text": "4", "row": 2, "column": 0},
+        {"text": "5", "row": 2, "column": 1},
+        {"text": "6", "row": 2, "column": 2},
+        {"text": "7", "row": 3, "column": 0},
+        {"text": "8", "row": 3, "column": 1},
+        {"text": "9", "row": 3, "column": 2},
+        {"text": "0", "row": 4, "column": 1},
+        {"text": "+", "row": 1, "column": 3},
+        {"text": "-", "row": 2, "column": 3},
+        {"text": "*", "row": 3, "column": 3},
+        {"text": "/", "row": 4, "column": 3},
+        {"text": "C", "row": 4, "column": 0},
+        {"text": "=", "row": 4, "column": 2},
+        ]
+
+        for button in buttons:
+            tk.Button(self.root, text=button["text"], height=2, width=6, font=("Arial", 18), command=lambda txt=button["text"]: self.button_click(txt)).grid(row=button["row"], column=button["column"])
 
     def button_click(self, text):
         if text == "C":
