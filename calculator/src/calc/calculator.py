2e45cec0b32ebbd85b43229a11af59dbc6ee4a56
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-05-02 16:25:45 +0300

    calculator.py docstring

--- calculator/src/calc/calculator.py
+++ calculator/src/calc/calculator.py
@@ -1,19 +1,64 @@
 class Calculator:
+    """
+    Luokka, joka pystyy suorittamaan yksinkertaisia laskutoimituksia.
+    Attributes:
+        number: nykyinen numero laskimessa
+        calculation: laskutoimitus, jota suoritetaan
+        result: laskutoimituksen vastaus
+    """
+
     def __init__(self):
+        """
+        Luokan konstruktori, joka luo numeron, laskutoimituksen ja vastauksen.
+
+        Args:
+            number: nykyinen numero laskimessa
+            calculation: laskutoimitus, jota suoritetaan
+            result: laskutoimituksen vastaus
+        """
+
         self.number = 0
         self.calculation = ""
         self.result = 0
 
     def add(self, num):
+        """
+        Lisää halutun numeron nykyiseen numeroon laskimessa.
+
+        Args:
+            num: nykyiseen numeroon lisättävä numero
+        """
+
         self.result = self.number + num
 
     def substract(self, num):
+        """
+        Vähentää halutun numeron nykyisestä numerosta laskimessa.
+
+        Args:
+            num: nykyisestä numertosta vähennettävä numero
+        """
+
         self.result = self.number - num
 
     def multiply(self, num):
+        """
+        Kertoo halutulla numerolla nykyisen numeron laskimessa.
+
+        Args:
+            num: numero, jolla kerrotaan nykyinen numero.
+        """
+
         self.result = self.number * num
 
     def divide(self, num):
+        """
+        Jakaa nykyisen numeron halutulla numerolla. Jos numero on 0, jako ei onnistu ja tulee virheviesti.
+
+        Args:
+            num: numero, jolla jaetaan nykyinen numero.
+        """
+
         if num != 0:
             self.result = self.number / num
         else:
@@ -21,9 +66,21 @@ class Calculator:
             print("Error: Cannot divide by zero")
 
     def clear(self):
+        """
+        Nollaa laskimen arvot.
+
+        """
+
         self.number = 0
         self.calculation = ""
         self.result = 0
 
     def get_result(self):
+        """
+        Palauttaa laskimessa lasketun laskutoimituksen.
+
+        Returns:
+            self.result, joka kertoo laskimessa lasketun vastauksen.
+        """
+
         return self.result
